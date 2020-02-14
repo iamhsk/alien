e51be6a12d41cf3077437aa75e39765175e4e784
iamhsk <helenkim@Helens-MacBook-Pro.local>
2020-02-14 15:01:22 -0800

    Added bullets and aliens

--- settings.py
+++ settings.py
@@ -7,4 +7,13 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230,230)
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bulles_allowed= 3
         
+        # Ship settings
+        self.ship_speed = 3.5
